0ffc490db9b8a1c5d1bc320c503f0af28f16d3ac
Marvin Toussaint <43975825+Marvintoussaint@users.noreply.github.com>
2022-04-05 09:19:32 -0400

    Time: 187 ms (57.68%), Space: 44.5 MB (36.84%) - LeetHub

---
+++ 707-design-linked-list/707-design-linked-list.cs
@@ -0,0 +1,106 @@
+public class MyLinkedList {
+    
+    public class Node
+    {
+       public  int val;
+        
+        public Node next;
+        
+        public Node(int val)
+        {
+            this.val = val;
+        }
+    }
+    
+    
+        Node head;
+
+        int size;
+
+    public MyLinkedList() {
+        head = new Node(-1);
+        size = 0;
+    }
+    
+    public int Get(int index) {
+                    if ( index >= size || index < 0)
+            {
+                return -1;
+            }
+
+            int count = 0;
+            Node curr = head;
+
+            while (count <= index)
+            {
+                curr = curr.next;
+                count++;
+             }
+
+            return curr.val;
+        
+    }
+    
+    public void AddAtHead(int val) {
+       AddAtIndex(0,val); 
+    }
+    
+    public void AddAtTail(int val) {
+      AddAtIndex(size,val);  
+    }
+    
+    public void AddAtIndex(int index, int val) {
+                    if (index > size)
+            {
+                return;
+            }
+
+            if (index < 0)
+            {
+                index = 0;
+            }
+
+            int count = 0;
+            Node curr = head;
+
+            while (count < index)
+            {
+                curr = curr.next;
+                count++;
+            }
+
+            Node node = new Node(val);
+            node.next = curr.next;
+            curr.next = node;
+            size ++;
+    }
+    
+    public void DeleteAtIndex(int index) {
+                    if (index >= size)
+            {
+                return;
+            }
+
+            int count = 0;
+            Node curr = head;
+
+            while (count < index)
+            {
+                curr = curr.next;
+                count++;
+            }
+            curr.next = curr.next.next;
+            size--;
+        
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.Get(index);
+ * obj.AddAtHead(val);
+ * obj.AddAtTail(val);
+ * obj.AddAtIndex(index,val);
+ * obj.DeleteAtIndex(index);
+ */
