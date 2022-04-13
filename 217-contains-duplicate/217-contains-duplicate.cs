166bc3dc239f3803c0e3c39ea6bcaa191301fc88
Marvin Toussaint <43975825+Marvintoussaint@users.noreply.github.com>
2022-04-13 11:40:07 -0400

    Time: 175 ms (87.61%), Space: 52.9 MB (5.78%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cs
+++ 217-contains-duplicate/217-contains-duplicate.cs
@@ -1,11 +1,17 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
 
-        Array.Sort(nums);
-        
-        for(int i = 0; i < nums.Length -1 ; i++)
+   Dictionary<int,int> d = new Dictionary<int,int>();
+        for(int i = 0; i < nums.Length; i++)
         {
-            if(nums[i] == nums[i + 1]) return true;
+            if(d.ContainsKey(nums[i]))
+            {
+                return true;
+            }
+            else
+            {
+                d[nums[i]] = 1;
+            }
         }
         return false;
     }
