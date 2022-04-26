76ffdbe45ddcce0625c2d207b7d4ff378e12bf6e
Marvin Toussaint <43975825+Marvintoussaint@users.noreply.github.com>
2022-04-26 11:08:17 -0400

    Time: 196 ms, Memory: 47 MB - LeetHub

---
+++ move-zeroes/move-zeroes.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public void MoveZeroes(int[] nums) {
+        
+        
+        // base case 
+        
+        int len = nums.Length;
+        
+        if(len < 2)
+        {
+            return;
+        }
+        
+        int left = 0;
+        int right = 1;
+        
+        while (right < len)
+        {
+            if(nums[left] != 0)
+            {
+                left ++;
+                right ++;
+            }else if (nums[right] == 0)
+            {
+                right++;
+            }else{
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+            }
+        }
+        }
+        
+    }
