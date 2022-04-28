a6ea9fabd5745afb3a5fb1b6f4a2424bafde4ecd
Marvin Toussaint <43975825+Marvintoussaint@users.noreply.github.com>
2022-04-28 09:46:36 -0400

    Time: 144 ms (90.35%), Space: 46.2 MB (8.82%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cs
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cs
@@ -2,24 +2,23 @@ public class Solution {
     public int RemoveDuplicates(int[] nums) {
         
         int len = nums.Length;
+        
         int left = 0;
+        
         int right = 1;
+        
         
         while (right < len)
         {
             if(nums[left] == nums[right])
             {
                 right++;
-            }else if(nums[left] != nums[right])
+            }else
             {
                 left++;
                 nums[left] = nums[right];
-            }else{
-                left++;
-                right++;
             }
         }
-        
         return left + 1;
     }
 }
