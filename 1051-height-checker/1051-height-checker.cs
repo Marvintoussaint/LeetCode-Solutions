bbdf134d0283fe6c1f8c621d9a3852e6b5882fc4
Marvin Toussaint <43975825+Marvintoussaint@users.noreply.github.com>
2022-05-11 11:49:14 -0400

    Time: 105 ms (54.19%), Space: 37.1 MB (57.74%) - LeetHub

--- 1051-height-checker/1051-height-checker.cs
+++ 1051-height-checker/1051-height-checker.cs
@@ -1,30 +1,28 @@
 public class Solution {
     public int HeightChecker(int[] heights) {
+        int counter = 0;
+        int len = heights.Length;
         
-            int len = heights.Length;
-            int counter = 0;
-
-            int[] expected = new int[len];
-
-            for (int i = 0; i < heights.Length; i++)
+        int[] expected = new int[len];
+        
+        for(int i = 0; i < len ; i++)
+        {
+            expected[i] = heights[i];
+        }
+        
+        Array.Sort(expected);
+        
+        
+        
+        for(int j = 0; j < expected.Length; j++)
+        {
+            if(expected[j] != heights[j])
             {
-                expected[i] = heights[i];
-             
+                counter++;
             }
-
-            Array.Sort(expected);
-
-            for (int j = 0; j < expected.Length; j++)
-            {
-                if (expected[j] != heights[j])
-                {
-                    counter++;
-                }
-            }
-
-            return counter;
+        }
         
-        
+        return counter;
         
     }
 }
