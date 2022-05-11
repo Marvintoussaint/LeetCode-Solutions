e53831d5c0ecbf82d387d4f9845bcc9c1598fd41
Marvin Toussaint <43975825+Marvintoussaint@users.noreply.github.com>
2022-05-11 09:33:31 -0400

    Time: 141 ms (33.51%), Space: 39.3 MB (82.37%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cs
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cs
@@ -1,16 +1,17 @@
 public class Solution {
     public bool CheckIfExist(int[] arr) {
+        int len = arr.Length;
         
-    for (int i = 0; i < arr.Length ; i++)
-    {
-        for (int j = i +1; j <  arr.Length; j++)
+        for ( int i = 0; i < len; i++)
         {
-            if(arr[i] == arr[j] *2 || 2* arr[i] == arr[j])
+            for (int j = i + 1 ; j < len ; j++)
             {
-                return true;
+                if(arr[i] == arr[j] * 2 || arr[i] * 2 == arr[j])
+                {
+                    return true;
+                }
             }
         }
-    }
         return false;
     }
 }
