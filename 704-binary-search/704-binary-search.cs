e26a719cc1480fed64ceb0273905cf7d16dae0ae
Marvin Toussaint <43975825+Marvintoussaint@users.noreply.github.com>
2022-05-17 14:04:32 -0400

    Time: 133 ms (74.99%), Space: 50.9 MB (13.00%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        
+        int start = 0;
+        
+        int end = nums.Length -1;
+        
+        while(start <= end )
+        {
+            int mid = start + (end - start) / 2;
+            
+            if(target < nums[mid])
+            {
+                end = mid -1;
+            } else if (target > nums[mid])
+            {
+                start = mid +1;
+            }else {
+                return mid;
+            }
+        }
+        
+        return -1;
+    }
+}
