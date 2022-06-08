77b4dcdd96e0e6b61fe8da0d29c7aff04e81adaf
Marvin Toussaint <43975825+Marvintoussaint@users.noreply.github.com>
2022-06-08 11:15:09 -0400

    Time: 287 ms (42.79%), Space: 47.9 MB (74.47%) - LeetHub

--- 15-3sum/15-3sum.cs
+++ 15-3sum/15-3sum.cs
@@ -1,54 +1,53 @@
 public class Solution {
     public IList<IList<int>> ThreeSum(int[] nums) {
-        var result = new List<IList<int>>();
- 
-        // Sort the array
+        
         Array.Sort(nums);
- 
-        // Set the target value
+      
+        var result = new List<IList<int>>();
+        
         var target = 0;
- 
-        // Loop through numbers
-        for (int index = 0; index < nums.Length; ++index) {
-            // Skip duplicates
-            if (index > 0 && nums[index] == nums[index - 1]) {
+        
+        int len = nums.Length; 
+        
+        for(int i = 0; i < len; i++ )
+        {
+            if(i > 0 && nums[i] == nums[i -1])
+            {
                 continue;
             }
- 
-            // Find all 3 sum combinations that equal the target value
-            var startIndex = index + 1;
-            var endIndex = nums.Length - 1;
-            while (startIndex < endIndex) {
-                // Get the sum
-                var sum = nums[index] + nums[startIndex] + nums[endIndex];
- 
-                // Sum equals target
-                if (sum == target) {
-                    // Add values to results when a target is found
-                    var valueStart = nums[startIndex];
-                    var valueEnd = nums[endIndex];
-                    result.Add(new List<int>{nums[index], valueStart, valueEnd});
- 
-                    // Advance past duplicate items to the 'next' values
-                    // at the start and end of the array
-                    while (startIndex < endIndex && valueStart == nums[startIndex]) {
-                        ++startIndex;
+            
+            int L = i + 1;
+            int R= len -1;
+            
+            while(L < R)
+            {
+                var sum = nums[i] + nums[L] + nums[R];
+                
+                if(sum == target)
+                {
+                    result.Add(new List<int> {nums[i],nums[L],nums[R]});
+                    L++;
+                    R--;
+                    
+                    while(L < R && nums[L] == nums[L-1])
+                    {
+                        L++;
                     }
-                    while (startIndex < endIndex && valueEnd == nums[endIndex]) {
-                        --endIndex;
+                    
+                    while (L < R && nums[R] == nums[R + 1])
+                    {
+                        R--;
                     }
- 
-                // Sum is less than target
-                } else if (sum < target) {
-                    ++startIndex;
- 
-                // Sum is greater than target
-                } else {
-                    --endIndex;
+                }else if (sum < target)
+                {
+                    L++;
+                }else {
+                    R--;
                 }
+                
             }
         }
- 
-        return result;
+        
+            return result;
     }
     }
