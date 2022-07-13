665f5e429f582f7c7df2da7d525c2790bcc26806
Marvin Toussaint <43975825+Marvintoussaint@users.noreply.github.com>
2022-07-13 07:19:23 -0400

    Time: 102 ms (83.57%), Space: 42.3 MB (6.03%) - LeetHub

---
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.cs
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode RemoveElements(ListNode head, int val) {
+          ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode pre = dummy, cur = head;
+        
+        while(cur != null){
+            if(cur.val == val){
+                pre.next = cur.next;
+            }else{
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+}
